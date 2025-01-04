5084790d633c9b94dbf575d57cab91213193542b
“Ali <tadayoni.ali.enayat@gmail.com>
2025-01-04 18:25:09 +0330

    -set up tailwind -make some meta data like title,description and favIcon

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "User Area",
+};
+
 export default function Page() {
   return <h1>account page</h1>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,20 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, loacted in the heart of the Itailan Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
+      <body className="min-h-screen text-primary-100 bg-primary-950">
         <header>
           <Logo />
           <Navigation />
