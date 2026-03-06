a15bd6d5400842123ce5281984696b7222a8dd98
“Ali <tadayoni.ali.enayat@gmail.com>
2026-03-06 15:43:32 +0330

    fix issues

--- app/layout.js
+++ app/layout.js
@@ -15,13 +15,16 @@ export const metadata = {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis",
   },
-  description: "Luxurious cabin hotel, loacted in the heart of the Itailan Dolomites, surrounded by beautiful mountains and dark forests",
+  description:
+    "Luxurious cabin hotel, loacted in the heart of the Itailan Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className={`${josefin.className} antialiased flex flex-col min-h-screen text-primary-100 bg-primary-950 relative`}>
+      <body
+        className={`${josefin.className} antialiased flex flex-col min-h-screen text-primary-100 bg-primary-950 relative`}
+      >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
           <main className="max-w-7xl w-full  mx-auto">
