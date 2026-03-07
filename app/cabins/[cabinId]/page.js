c4c60c4693e26cd19b48c775f0734542f2bc9c56
“Ali <tadayoni.ali.enayat@gmail.com>
2026-03-07 21:04:30 +0330

    add line breaks to improve readability

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -10,6 +10,7 @@ export async function generateMetadata({ params }) {
     title: `Cabin ${name}`,
   };
 }
+
 export async function generateStaticParams() {
   const cabins = await getCabins();
   const ids = cabins.map((cabin) => ({
@@ -25,7 +26,9 @@ export default async function Page({ params }) {
     <div className="max-w-6xl mx-auto mt-8">
       <Cabin cabin={cabin} />
       <div>
-        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">Reserve {cabin.name} today. Pay on arrival.</h2>
+        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+          Reserve {cabin.name} today. Pay on arrival.
+        </h2>
         <Suspense fallback={<Spinner />}>
           <Reservation cabin={cabin} />
         </Suspense>
