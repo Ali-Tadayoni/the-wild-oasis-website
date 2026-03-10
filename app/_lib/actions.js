d1ea7944ac07a164181da2b45811852f576efbc7
“Ali <tadayoni.ali.enayat@gmail.com>
2026-03-10 13:21:19 +0330

    add logout functionality

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,11 +1,17 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 // The functoins in this file are never leaked to client.
 
 export async function signInAction() {
   await signIn("google", {
     redirectTo: "/account",
+  });
+}
+
+export async function signOutAction() {
+  await signOut({
+    redirectTo: "/",
   });
 }
