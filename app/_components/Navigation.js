b54ed15a1ec9f368b67d9290d92334305e99352c
“Ali <tadayoni.ali.enayat@gmail.com>
2026-03-10 12:46:57 +0330

    implement basic authentication

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import { Span } from "next/dist/trace";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+  console.log("session", session);
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex-center gap-16 ">
@@ -13,8 +18,19 @@ export default function Navigation() {
           <Link href="/about">About</Link>
         </li>
         <li className="hover:text-accent-400 transition-colors">
-          {" "}
-          <Link href="/account">Guest area</Link>
+          {session?.user?.image ? (
+            <Link href="/account" className="flex items-center gap-4">
+              <img
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link href="/account">Guest area</Link>
+          )}
         </li>
       </ul>
     </nav>
