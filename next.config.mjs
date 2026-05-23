c49e6e0cbda1664e1c0ee04bc2995487c27daa32
“Ali <tadayoni.ali.enayat@gmail.com>
2026-05-23 15:00:53 +0330

    api: integrate with new supabase service

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pjoxljefctmxekvudtuu.supabase.co",
+        hostname: "uhsaxmtcflukdxpgqowo.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabins/**",
         search: "",
